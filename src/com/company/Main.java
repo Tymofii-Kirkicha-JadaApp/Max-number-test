c07cc50c23c8f074fb0c06fb36db8e9dc195c3af
Tymofii Kirkicha <tymofii@jadaapp.com>
2022-10-06 21:54:11 +0300

    Fix Bug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        calculateMaxValue();
+        calculateMaxValue();
     }
 
     private static void calculateMaxValue() {
